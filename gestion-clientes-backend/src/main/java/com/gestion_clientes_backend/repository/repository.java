a88521a3f6a31955c50f2df98793af10bed1d359
leo moises nisperuza amaya <leomoisesnisperuzaamaya7@gmail.com>
2025-04-03 20:00:30 -0500

    Delete gestion-clientes-backend directory

--- gestion-clientes-backend/src/main/java/com/gestion_clientes_backend/repository/repository.java
+++
@@ -1,8 +0,0 @@
-package com.gestion_clientes_backend.repository;
-
-import com.gestion_clientes_backend.models.models;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repository extends JpaRepository<models, Long> {
-
-}
